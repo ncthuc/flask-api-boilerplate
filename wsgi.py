5e6be1bd2ec2ae3785d7e0d9e972b6199ae8e770
thucnguyen <gthuc.nguyen@gmail.com>
2019-10-17 10:50:14 +0700

    add wsgi.py entry point, update packages version, add .env.sample

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+import logging
+from main import app as application
+
+__author__ = 'ThucNC'
+_logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    application.run()
